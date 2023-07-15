781683229a9a9529999b1bcbd4289f9911c2ed06
Роман Соколов <sokoloff.rv@gmail.com>
2023-07-15 19:48:39 +0600

    Удалены комментарии и неиспользуемый метод

--- models/RegisterForm.php
+++ models/RegisterForm.php
@@ -19,19 +19,12 @@ class RegisterForm extends Model
     public function rules()
     {
         return [
-            // все поля обязательны
             [['name', 'email', 'password', 'password_repeat'], 'required'],
-            // имя не должно содержать цифр и специальных символов, кроме пробела
             [['name'], 'match', 'pattern' => '/^[a-zA-Zа-яА-ЯёЁ\s\-]*$/', 'message' => 'Имя и фамилия заполнены некорректно.'],
-            // email должен быть валидным адресом электронной почты
             ['email', 'email'],
-            // email должен быть уникальным
             ['email', 'unique', 'targetClass' => '\app\models\User', 'message' => 'Этот адрес электронной почты уже занят.'],
-            // пароль должен содержать не менее 6 символов
             ['password', 'string', 'min' => 6],
-            // пароль и повтор пароля должны совпадать
             ['password_repeat', 'compare', 'compareAttribute' => 'password', 'message' => 'Пароли не совпадают.'],
-            // avatar должен быть файлом изображения
             ['avatar', 'file', 'extensions' => 'png, jpg, jpeg, gif'],
         ];
     }
@@ -45,21 +38,5 @@ class RegisterForm extends Model
             'password_repeat' => 'Повторите пароль',
             'avatar' => 'Аватар',
         ];
-    }
-
-    public function register()
-    {
-        if (!$this->validate()) {
-            return null;
-        }
-        
-        $user = new User();
-        $user->name = $this->name;
-        $user->email = $this->email;
-        $user->setPassword($this->password);
-        $user->avatar = $this->avatar;
-        $user->generateAuthKey();
-        
-        return $user->save();
     }
 }
