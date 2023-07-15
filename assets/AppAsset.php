8b9b26c60c442c1072f44ea8e2c3b5e15170db56
Роман Соколов <sokoloff.rv@gmail.com>
2023-07-15 20:08:41 +0600

    Поправлены ассеты

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -23,6 +23,8 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/vendor.js',
+        'js/main.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
