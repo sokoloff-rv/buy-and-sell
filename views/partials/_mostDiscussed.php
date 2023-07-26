e305d8266657188c907bcf4d570db4ff4ba52153
Роман Соколов <sokoloff.rv@gmail.com>
2023-07-26 14:38:16 +0600

    Карточка объявления вынесена в отдельный шаблон

--- views/partials/_mostDiscussed.php
+++ views/partials/_mostDiscussed.php
@@ -1,9 +1,6 @@
 <?php
 
-use yii\helpers\Html;
-use yii\helpers\Url;
 use app\models\Offer;
-use app\models\Image;
 
 $offers = Offer::getMostDiscussed();
 
@@ -18,32 +15,7 @@ if (count($offers) > 0) :
         </div>
         <ul>
             <?php foreach ($offers as $offer) : ?>
-                <li class="tickets-list__item">
-                    <div class="ticket-card">
-                        <div class="ticket-card__img">
-                            <?= Html::img($offer->images[0]->image_path, ['srcset' => $offer->images[0]->image_path . ' 2x', 'alt' => 'Изображение товара']) ?>
-                        </div>
-                        <div class="ticket-card__info">
-                            <span class="ticket-card__label"><?= Html::encode($offer->label) ?></span>
-                            <div class="ticket-card__categories">
-                                <?php foreach ($offer->categories as $category) : ?>
-                                    <?= Html::a(Html::encode($category->name), ['offers/category', 'id' => $category->id]) ?>
-                                <?php endforeach; ?>
-                            </div>
-                            <div class="ticket-card__header">
-                                <h3 class="ticket-card__title">
-                                    <?= Html::a(Html::encode($offer->title), ['offers/view', 'id' => $offer->id]) ?>
-                                </h3>
-                                <p class="ticket-card__price">
-                                    <span class="js-sum"><?= Html::encode(round($offer->price, 0)) ?></span> ₽
-                                </p>
-                            </div>
-                            <div class="ticket-card__desc">
-                                <p><?= Html::encode($offer->description) ?></p>
-                            </div>
-                        </div>
-                    </div>
-                </li>
+                <?= $this->render('_offerCard', ['offer' => $offer]) ?>
             <?php endforeach; ?>
         </ul>
     </div>
