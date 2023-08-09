1a2f977890b15070310b9ccfb6b2be532b4e406a
Роман Соколов <sokoloff.rv@gmail.com>
2023-08-09 16:31:40 +0600

    Добавлена страница с комментариями

--- controllers/MyController.php
+++ controllers/MyController.php
@@ -2,7 +2,10 @@
 
 namespace app\controllers;
 
+use Yii;
+use yii\web\ForbiddenHttpException;
 use yii\web\Controller;
+use app\models\Offer;
 
 class MyController extends AccessController
 {
@@ -13,6 +16,25 @@ class MyController extends AccessController
 
     public function actionComments()
     {
-        return $this->render('comments');
+        $offers = Offer::find()->where(['user_id' => Yii::$app->user->id])->all();
+
+        $offersWithComments = [];
+        foreach ($offers as $offer) {
+            $comments = $offer->comments;
+            if (!empty($comments)) {
+                $offersWithComments[] = [
+                    'offer' => $offer,
+                    'comments' => $comments
+                ];
+            }
+        }
+
+        usort($offersWithComments, function ($a, $b) {
+            return end($b['comments'])->created_at <=> end($a['comments'])->created_at;
+        });
+
+        return $this->render('comments', [
+            'offersWithComments' => $offersWithComments
+        ]);
     }
 }
--- views/my/comments.php
+++ views/my/comments.php
@@ -5,4 +5,47 @@
 $this->title = 'Комментарии';
 ?>
 
-<h1>Комментарии</h1>
+<section class="comments">
+    <div class="comments__wrapper">
+        <h1 class="visually-hidden">Страница комментариев</h1>
+        <?php foreach ($offersWithComments as $data) : ?>
+            <div class="comments__block">
+                <div class="comments__header">
+                    <a href="#" class="announce-card">
+                        <h2 class="announce-card__title">
+                            <?= $data['offer']->title ?>
+                        </h2>
+                        <span class="announce-card__info">
+                            <span class="announce-card__price">₽ 
+                                <?= round($data['offer']->price, 0) ?>
+                            </span>
+                            <span class="announce-card__type"><?= $data['offer']->label ?></span>
+                        </span>
+                    </a>
+                </div>
+                <ul class="comments-list">
+                    <?php foreach ($data['comments'] as $comment) : ?>
+                        <li class="js-card">
+                            <div class="comment-card">
+                                <div class="comment-card__header">
+                                    <a href="#" class="comment-card__avatar avatar">
+                                        <img src="/<?= $comment->user->avatar ?>" alt="Аватар пользователя">
+                                    </a>
+                                    <p class="comment-card__author">
+                                        <?= $comment->user->name ?>
+                                    </p>
+                                </div>
+                                <div class="comment-card__content">
+                                    <p>
+                                        <?= $comment->text ?>
+                                    </p>
+                                </div>
+                                <button class="comment-card__delete js-delete" type="button">Удалить</button>
+                            </div>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</section>
