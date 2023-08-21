e34dc6d62177b2ba3295d0d7107366d928802f3f
Роман Соколов <sokoloff.rv@gmail.com>
2023-08-21 14:50:49 +0600

    Добавлено изменение окончания результатов поиска

--- views/search/index.php
+++ views/search/index.php
@@ -3,12 +3,16 @@
 /* @var $this yii\web\View */
 
 $this->title = 'Поиск';
+
+$foundCount = $pagination->totalCount;
+$foundWord = Yii::$app->i18n->format('{n, plural, =1{Найдена} one{Найдена} few{Найдено} many{Найдено} other{Найдено}}', ['n' => $foundCount], 'ru_RU');
+$publicationWord = Yii::$app->i18n->format('{n, plural, =1{публикация} one{публикация} few{публикации} many{публикаций} other{публикаций}}', ['n' => $foundCount], 'ru_RU');
 ?>
 
 <section class="search-results">
     <h1 class="visually-hidden">Результаты поиска</h1>
     <div class="search-results__wrapper">
-        <p class="search-results__label">Найдено <span class="js-results"><?= $pagination->totalCount ?> публикации</span></p>    
+    <p class="search-results__label"><?= $foundWord ?> <span class="js-results"><?= $foundCount ?> <?= $publicationWord ?></span></p>
         <ul class="search-results__list">
             <?php foreach ($offers as $offer) : ?>
                 <li class="search-results__item">
