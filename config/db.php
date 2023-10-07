cd86d49a6d03cd2938a3956eb14f5dbd30d9285b
sokoloff-rv <sokoloff.rv@gmail.com>
2023-10-07 16:32:48 +0300

    Обновлен .gitignore

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=kupiprodai',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=localhost;dbname=sokolo9w_tskfrce',
+    'username' => 'sokolo9w_buysell',
+    'password' => 'RBxk9I6&',
     'charset' => 'utf8',
 ];
